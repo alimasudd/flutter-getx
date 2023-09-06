7228e5ca19ddbf66dbd93354e1dd6cd9591ffcbf
alim <alimasudd.dev@gmail.com>
2023-09-06 09:31:48 +0700

    add simple variable and function to increment and decrement

--- lib/controller/controller_home.dart
+++ lib/controller/controller_home.dart
@@ -4,9 +4,22 @@ class HomeController extends GetxController{
   //reactive
   var dataPantau = 0.obs;
 
+  //simple
+  int dataPantauSimple = 0;
+
   //reactive function
   void increment() => dataPantau++;
   void decrement() => dataPantau--;
+
+  //simple function
+  void incrementSimple() {
+    dataPantauSimple++;
+    update();
+  }
+  void decrementSimple() {
+    dataPantauSimple--;
+    update();
+  }
 
 
 }
