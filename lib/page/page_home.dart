ebe9b4d4c5e4c84a352ac25cadc9bbd211ee34ac
alim <alimasudd.dev@gmail.com>
2023-09-06 09:34:36 +0700

    add getX put from class controller_home.dart

--- lib/page/page_home.dart
+++ lib/page/page_home.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx/controller/controller_home.dart';
 
 class PageHome extends StatelessWidget {
   PageHome({Key? key}) : super(key: key);
+
+  final homeC = Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
