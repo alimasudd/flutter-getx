94f5011d7306f328929337e4b5eb331c3075116c
alim <alimasudd.dev@gmail.com>
2023-09-06 09:42:30 +0700

    refactor rename page_home.dart to page_reactive_simple.dart

--- lib/page/page_reactive_simple.dart
+++ lib/page/page_reactive_simple.dart
@@ -2,16 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:getx/controller/controller_home.dart';
 
-class PageHome extends StatelessWidget {
-  PageHome({Key? key}) : super(key: key);
+class ReactiveSimple extends StatelessWidget {
+  ReactiveSimple({Key? key}) : super(key: key);
 
   final homeC = Get.put(HomeController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-      ),
     );
   }
 }
