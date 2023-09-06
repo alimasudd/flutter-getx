d9c3dc466c7ddf41fd7b6a110abb713b02fe70f1
alim <alimasudd.dev@gmail.com>
2023-09-06 09:26:10 +0700

    add view pageHome

---
+++ lib/page/page_home.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class PageHome extends StatelessWidget {
+  PageHome({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+      ),
+    );
+  }
+}
