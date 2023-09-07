5294f1bef05ec5e0841c58a694a76304da72c256
alim <alimasudd.dev@gmail.com>
2023-09-07 16:49:30 +0700

    create scaffold with body Center widget

--- lib/page/page_reactive_simple.dart
+++ lib/page/page_reactive_simple.dart
@@ -10,6 +10,9 @@ class ReactiveSimple extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('Reactive | Simple'),),
+      body: Center(
+      ),
     );
   }
 }
