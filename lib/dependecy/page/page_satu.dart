eba69169f40daf8c3b8092057dea5dacd042f092
alim <alimasudd.dev@gmail.com>
2023-09-10 09:03:36 +0700

    add appbar in page_satu.dart

--- lib/dependecy/page/page_satu.dart
+++ lib/dependecy/page/page_satu.dart
@@ -6,6 +6,9 @@ class PageSatu extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Satu'),
+      ),
     );
   }
 }
