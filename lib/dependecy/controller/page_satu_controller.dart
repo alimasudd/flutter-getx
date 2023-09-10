407724324c0c42c770032029c303af86f185b65c
alim <alimasudd.dev@gmail.com>
2023-09-10 08:59:18 +0700

    add map collections with value

--- lib/dependecy/controller/page_satu_controller.dart
+++ lib/dependecy/controller/page_satu_controller.dart
@@ -2,4 +2,6 @@
 import 'package:get/get.dart';
 
 class PageSatuController extends GetxController{
+  Map<String, dynamic> data = {'name': 'Siti', 'age': 30};
+  Map<String, dynamic> data2 = {'name': 'Budi', 'age': 45};
 }
