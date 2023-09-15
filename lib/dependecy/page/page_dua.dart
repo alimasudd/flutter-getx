ff7b2faa61911915de9a80eb14eacce496efc422
alimasudd <alimasudd.dev@gmail.com>
2023-09-15 20:36:32 +0700

    add text with observe variable and get.find getX

--- lib/dependecy/page/page_dua.dart
+++ lib/dependecy/page/page_dua.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getx/dependecy/controller/page_satu_controller.dart';
 
 class PageDua extends StatelessWidget {
   PageDua({Key? key}) : super(key: key);
+
+  PageSatuController pageDuaC = Get.find();
 
   @override
   Widget build(BuildContext context) {
@@ -13,6 +17,10 @@ class PageDua extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            Text(
+              '${pageDuaC.data2['name']} - ${pageDuaC.data2['age']}',
+              style: TextStyle(fontSize: 30),
+            ),
           ],
         ),
       ),
