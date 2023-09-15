d3ae6a5a9db39f2e4e51b2e8228daa9c82a7de9b
alimasudd <alimasudd.dev@gmail.com>
2023-09-15 20:33:03 +0700

    add column and appBar in page_dua.dart

--- lib/dependecy/page/page_dua.dart
+++ lib/dependecy/page/page_dua.dart
@@ -6,6 +6,16 @@ class PageDua extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Page Dua'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+          ],
+        ),
+      ),
     );
   }
 }
