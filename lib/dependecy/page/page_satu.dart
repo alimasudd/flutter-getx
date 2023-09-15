24a17e04b88eac93d669f88c16ebeb7e330cf49f
alimasudd <alimasudd.dev@gmail.com>
2023-09-15 20:32:40 +0700

    add column and button in page_satu.dart

--- lib/dependecy/page/page_satu.dart
+++ lib/dependecy/page/page_satu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:getx/dependecy/page/page_dua.dart';
 
 class PageSatu extends StatelessWidget {
   PageSatu({Key? key}) : super(key: key);
@@ -8,6 +9,22 @@ class PageSatu extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Page Satu'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => PageDua()));
+                },
+                child: Text('Page Dua >'))
+          ],
+        ),
       ),
     );
   }
