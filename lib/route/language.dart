4baa2ca182c0ae7dfb997089c56d293edf2a2085
alimasudd <alimasudd.dev@gmail.com>
2023-10-04 16:44:15 +0700

    add map collection language

--- lib/route/language.dart
+++ lib/route/language.dart
@@ -3,4 +3,13 @@ import 'package:get/get.dart';
 
 class Language extends Translations {
 
+  Map<String, Map<String, String>> get keys => {
+    'en_US' : {
+      'title' : 'English'
+    },
+    'id_ID' : {
+      'title' : 'Indonesia'
+    }
+  };
+
 }
