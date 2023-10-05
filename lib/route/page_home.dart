870fe0daed8c3d9b22049732f7ac61b0cf7270b5
alimasudd <alimasudd.dev@gmail.com>
2023-10-05 14:01:02 +0700

    implement localization in page home

--- lib/route/page_home.dart
+++ lib/route/page_home.dart
@@ -4,13 +4,13 @@ import 'package:getx/route/page_one.dart';
 import 'package:getx/route/page_two.dart';
 
 class PageHome extends StatelessWidget {
-  const PageHome({Key? key}) : super(key: key);
+  PageHome({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Page Home'),
+        title: Text('title'.tr),
       ),
       body: Center(
         child: Column(
@@ -18,7 +18,8 @@ class PageHome extends StatelessWidget {
           children: [
             ElevatedButton(
                 onPressed: (){
-                  Get.to(PageOne());
+                  Get.updateLocale(Locale('en'));
+                  // Get.to(PageOne());
                 },
                 child: Text('page one')),
             ElevatedButton(
